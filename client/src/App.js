071f3b8d6662e25c1cad842b7dc481763e5f53ae
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-12-24 12:38:58 +0500

    files updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import MobileLogo from './Components/MobileLogo/MobileLogo'
+import Navbar from './Components/Navbar/Navbar'
+import MobileNavbar from './Components/NavbarMobile/MobileNav'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar />
+      <MobileLogo />
+      <MobileNavbar />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
