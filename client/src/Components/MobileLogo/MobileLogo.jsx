012852c14b2a5334f4c74aab1b1a86fa759871f5
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-12-24 12:38:22 +0500

    MobileLogo component yozildi

---
+++ client/src/Components/MobileLogo/MobileLogo.jsx
@@ -0,0 +1,12 @@
+import './MobileLogo.css'
+
+const MobileLogo = () => {
+  return (
+    <div id='Logo_mobile'>
+      <p className='mobile_logo'>
+        Yamin ideal market <span className='dot_span'>©</span>
+      </p>
+    </div>
+  )
+}
+export default MobileLogo
