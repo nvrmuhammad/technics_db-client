b0e6595aab7c398ded360d39b29d79315f65fdd0
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-12-28 17:26:59 +0500

    updated App js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,38 @@
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
+import LoginAdmin from './Components/LoginAdmin/LoginAdmin'
+import LoginUser from './Components/LoginUser/LoginUser'
 import MobileLogo from './Components/MobileLogo/MobileLogo'
 import Navbar from './Components/Navbar/Navbar'
 import MobileNavbar from './Components/NavbarMobile/MobileNav'
+import Routing from './Routes/Routing'
+import Product from './Components/Product/Product'
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
+    <>
+      <div className='App'>
+        {/* <Navbar />
       <MobileLogo />
-      <MobileNavbar />
-    </div>
+      <MobileNavbar /> */}
+        {/* <LoginUser /> */}
+        {/* <LoginAdmin /> */}
+        <Routing />
+      </div>
+      <ToastContainer
+        position='top-right'
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='dark'
+      />
+    </>
   )
 }
 
