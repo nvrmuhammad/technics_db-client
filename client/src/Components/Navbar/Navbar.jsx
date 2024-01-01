17444082949c6d8dd3468b79af25497b933688eb
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2024-01-01 13:21:58 +0500

    Update Navbar.jsx

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,67 @@
 import { Link } from 'react-router-dom'
 import './Navbar.css'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [activeLink, setActiveLink] = useState(null)
+
+  const handleLinkClick = (linkId) => {
+    setActiveLink(linkId)
+  }
+
   return (
     <div id='Navbar_pc'>
       <div className='logo_navbar_pc'>
-        <p className='logotip_logo'>Yamin ideal market <span className='dot_span'>©</span></p>
+        <p className='logotip_logo'>
+          Yamin ideal market <span className='dot_span'>©</span>
+        </p>
       </div>
 
       <div className='navigators_navbar_pc'>
-        <Link className='navigator_navbar_pc'>Mahsulotlar</Link>
-        <Link className='navigator_navbar_pc'>Foydalanuvchilar</Link>
-        <Link className='navigator_navbar_pc'>Mahsulot qo'shish</Link>
-        <Link className='navigator_navbar_pc'>Foydalanuvchi qo'shish</Link>
+        <Link
+          to={'/admin/products'}
+          onClick={(e) => handleLinkClick(1)}
+          // className='navigator_navbar_pc'
+          className={
+            activeLink === 1
+              ? 'active_navbar_desktop navigator_navbar_pc'
+              : 'navigator_navbar_pc'
+          }
+        >
+          Mahsulotlar
+        </Link>
+        <Link
+          to={'/admin/users'}
+          onClick={(e) => handleLinkClick(2)}
+          className={
+            activeLink === 2
+              ? 'active_navbar_desktop navigator_navbar_pc'
+              : 'navigator_navbar_pc'
+          }
+        >
+          Foydalanuvchilar
+        </Link>
+        <Link
+          onClick={(e) => handleLinkClick(3)}
+          className={
+            activeLink === 3
+              ? 'active_navbar_desktop navigator_navbar_pc'
+              : 'navigator_navbar_pc'
+          }
+        >
+          Mahsulot qo'shish
+        </Link>
+
+        <Link
+          onClick={(e) => handleLinkClick(5)}
+          className={
+            activeLink === 5
+              ? 'active_navbar_desktop navigator_navbar_pc'
+              : 'navigator_navbar_pc'
+          }
+        >
+          Hamkorlar
+        </Link>
       </div>
     </div>
   )
